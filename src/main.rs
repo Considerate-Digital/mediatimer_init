606b018d596a207aa007b6015f29a19775e1f1b6
Alex McCartney <alexmccartney@protonmail.com>
2025-07-21 13:02:59 +0100

    adds pro run task feature

--- src/main.rs
+++ src/main.rs
@@ -530,7 +530,151 @@ fn run_task(task_list: Arc<Mutex<Vec<RunningTask>>>, task: Arc<Mutex<Task>>) {
     // stop the task after launching the new task to ensure a smooh overlap
     let _stopped_task = stop_task(task_list_clone_two.clone());
 }
+#[cfg(feature="pro")]
+fn run_task(task_list: Arc<Mutex<Vec<RunningTask>>>, task: Arc<Mutex<Task>>) {
+    let task_list_clone = Arc::clone(&task_list);
+    let task_list_clone_two = Arc::clone(&task_list);
 
+    log_info(format!("Run task: {:?}", task.lock().unwrap()).as_str());
+    
+    let looper = match task.lock().unwrap().auto_loop {
+        Autoloop::Yes => Autoloop::Yes,
+        Autoloop::No => Autoloop::No
+    };
+    let file_binding = task.lock().unwrap().file.clone();
+    let file = String::from(file_binding.to_str().unwrap());
+    let slide_delay = task.lock().unwrap().slide_delay.to_string();
+
+    match task.lock().unwrap().proc_type {
+        ProcType::Video => {
+            match looper {
+                Autoloop::Yes => {
+                    thread::spawn(move || {
+                        let child = Command::new("ffplay")
+                            .arg("-hide_banner")
+                            .arg("-loglevel")
+                            .arg("error")
+                            .arg("-fs")
+                            .arg("-loop")
+                            .arg("-1")
+                            .arg(&file)
+                            .spawn().expect("no child");
+
+                        let running_task = RunningTask::new(child, false);
+                        task_list_clone.lock().unwrap().push(running_task);
+                    });
+
+                }
+                Autoloop::No => {
+                    thread::spawn(move || {
+                        let child = Command::new("ffplay")
+                            .arg("-hide_banner")
+                            .arg("-loglevel")
+                            .arg("error")
+                            .arg("-fs")
+                            .arg(&file)
+                            .spawn().expect("no child");
+
+                        let running_task = RunningTask::new(child, false);
+                        task_list_clone.lock().unwrap().push(running_task);
+                    });
+                }
+            };
+        },
+        ProcType::Audio => {
+            match looper {
+                Autoloop::Yes => {
+                    thread::spawn(move || {
+                        let child = Command::new("ffplay")
+                            .arg("-hide_banner")
+                            .arg("-loglevel")
+                            .arg("error")
+                            //.arg("-fs")
+                            .arg("-loop")
+                            .arg("-1")
+                            .arg(&file)
+                            .spawn().expect("no child");
+
+                        let running_task = RunningTask::new(child, false);
+                        task_list_clone.lock().unwrap().push(running_task);
+                    });
+
+                }
+                Autoloop::No => {
+                    thread::spawn(move || {
+                        let child = Command::new("ffplay")
+                            .arg("-hide_banner")
+                            .arg("-loglevel")
+                            .arg("error")
+                            //.arg("-fs")
+                            .arg(&file)
+                            .spawn().expect("no child");
+
+                        let running_task = RunningTask::new(child, false);
+                        task_list_clone.lock().unwrap().push(running_task);
+                    });
+                }
+            };
+        },
+        ProcType::Image => {
+            thread::spawn(move || {
+                let child = Command::new("feh")
+                    .arg("-YxqFZz")
+                    .arg("-B")
+                    .arg("black")
+                    .arg(&file)
+                    .spawn().expect("no child");
+                let running_task = RunningTask::new(child, false);
+                task_list_clone.lock().unwrap().push(running_task);
+            });
+        },
+        ProcType::Slideshow => {
+            thread::spawn(move || {
+                let child = Command::new("feh")
+                    .arg("-YxqFZz")
+                    .arg("-B")
+                    .arg("black")
+                    .arg("-D")
+                    .arg(&slide_delay)
+                    .arg(&file)
+                    .spawn().expect("no child");
+                let running_task = RunningTask::new(child, false);
+                task_list_clone.lock().unwrap().push(running_task);
+            });
+        },
+        ProcType::Browser => {
+            thread::spawn(move || {
+                let child = Command::new("chromium")
+                    //.arg("--user-data-dir=/tmp/chromium/")
+                    //.arg("--disable-session-crashed-bubble")
+                    .arg("--disable-infobars")
+                    //.arg("--kiosk")
+                    .arg("--incognito")
+                    .arg("--start-fullscreen")
+                    .arg("--start-maximized")
+                    .arg(&file)
+                    .spawn().expect("no child");
+
+                        let running_task = RunningTask::new(child, false);
+                    task_list_clone.lock().unwrap().push(running_task);
+            });
+
+        },
+        ProcType::Executable => {
+            thread::spawn(move || {
+                let child = Command::new("sh")
+                    .arg(&file)
+                    .spawn().expect("no child");
+                        let running_task = RunningTask::new(child, false);
+                    task_list_clone.lock().unwrap().push(running_task);
+            });
+
+        }
+    }
+
+    // stop the task after launching the new task to ensure a smooh overlap
+    let _stopped_task = stop_task(task_list_clone_two.clone());
+}
 fn stop_task(task_list: Arc<Mutex<Vec<RunningTask>>>) {
         if task_list.lock().unwrap().len() > 0 {
 
